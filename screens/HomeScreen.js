ea3d6ff981e8de68789f98cd3c66cd101e74b4be
Umer Shamaan <umer.shamaan@ug.bilkent.edu.tr>
2021-04-25 22:54:49 +0300

    cutom url length check

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
 import Card from '../components/UI/Card';
 import { Ionicons, AntDesign } from '@expo/vector-icons';
-import { Input, CheckBox, Button } from 'react-native-elements';
+import { Input, CheckBox, Button, Overlay} from 'react-native-elements';
 import * as authActions from '../store/actions/auth';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -15,60 +15,70 @@ const HomeScreen = props => {
     const [customUrlChecked, setCostumUrlChecked] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [customUrl, setCustomUrl] = useState('');
+    const [overlayVisible, setOverlayVisible] = useState(false);
+    const [overlayMessage, setOverlayMessage] = useState('');
     const userId = useSelector(state => state.auth.userId);
 
     const dispatch = useDispatch();
 
 
     const generateShortUrlHandler = async (text, isCostumUrl, myCustomUrl) => {
-        setIsLoading(true);
-        console.log(`long url : ${text}`);
-        console.log(`custom url : ${myCustomUrl}`);
-        //send api request to genetate the long url
-        let response;
-        if(isCostumUrl){
-            response = await fetch(
-                'https://teenyurl21.herokuapp.com/api/Url/Custom',
-                {
-                    method: 'Post',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        userID: userId,
-                        longURL: text,
-                        shortURL: myCustomUrl
-                    })
-                }
-            );
+
+        if(myCustomUrl.length <= 0 && isCostumUrl){
+            setOverlayMessage("Please enter a Costum Url");
+            setOverlayVisible(true);
         }
+        else if(myCustomUrl.length > 10 && isCostumUrl){
+            setOverlayMessage("The url cannot be more than 10 characters long!");
+            setOverlayVisible(true);
+        }
+
         else{
-            response = await fetch(
-                'https://teenyurl21.herokuapp.com/api/Url',
-                {
-                    method: 'Post',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        userID: userId,
-                        longURL: text
-                    })
-                }
-            );
-        }
-       
-        if (!response.ok) {
-            const errorResData = await response.json();
-            const errorId = errorResData.error.message;
-            let message = 'Something went wrong!';
-            console.log(message);
-            console.log(errorId);
+            setIsLoading(true);
+            //send api request to genetate the long url
+            let response;
+            if(isCostumUrl){
+                response = await fetch(
+                    'https://teenyurl21.herokuapp.com/api/Url/Custom',
+                    {
+                        method: 'Post',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            userID: userId,
+                            longURL: text,
+                            shortURL: myCustomUrl
+                        })
+                    }
+                );
+            }
+            else{
+                response = await fetch(
+                    'https://teenyurl21.herokuapp.com/api/Url',
+                    {
+                        method: 'Post',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            userID: userId,
+                            longURL: text
+                        })
+                    }
+                );
+            }
+        
+            if (!response.ok) {
+                const errorResData = await response.json();
+                const errorId = errorResData.error.message;
+                let message = 'Something went wrong!';
+                setIsLoading(false);
+            }
+            const resData = await response.json();
+            console.log(resData);
             setIsLoading(false);
         }
-        const resData = await response.json();
-        console.log(resData);
-        setIsLoading(false);
     }
 
     const logoutHandler = () => {
@@ -77,6 +87,11 @@ const HomeScreen = props => {
 
     return(
         <View style={styles.screen}>
+            <View>
+                <Overlay isVisible={overlayVisible} onBackdropPress={() => {setOverlayVisible(false)}}>
+                    <Text>{overlayMessage}</Text>
+                </Overlay>
+            </View>
             <Card style = {styles.cardContainer}>
                 <Input
                     placeholder='ENTER LONG URL'
